6a7fe6bcd09d46ddba385e9cfe86deefc0011178
amitfe <amitfe1@gmail.com>
2024-03-22 10:37:00 +0200

    start of model implementation

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn as nn
 
 def deep_supervision_loss(preds, label, base_loss_function, deep_layers_weight_factor=1):
+
     if preds.dim() > label.dim():
         preds = torch.unbind(preds, dim=1)
     else:
@@ -23,3 +25,36 @@ def deep_supervision_loss(preds, label, base_loss_function, deep_layers_weight_f
         raise(ValueError(f"loss should be non negative but got loss={loss}"))
 
     return loss
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        return self.conv(x)
+
+class UpConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UpConvBlock, self).__init__()
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv3d(in_channels, out_channels,kernel_size=3,stride=1,padding=1,bias=True),
+		    nn.BatchNorm3d(out_channels),
+			nn.ReLU(inplace=True)
+        )
+
+    def forward(self,x):
+        x = self.up(x)
+        return x
+
+
+class AttentionUNET(nn.Module):
+    def __init__(self):
+        super(AttentionUNET, self).__init__()
